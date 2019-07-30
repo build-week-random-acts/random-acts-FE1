f88475fd0491a955d391705986a578276b26af8b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-30 13:30:54 -0500

    linked contact list and contact card; created placeholder card

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
-const ContactCard = (props) => {
-    const { contact } = props;
-    return(
-        <div>
+const ContactCard = props => {
+  const { contact } = props;
+  return (
+    <Accordion>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          <div>{contact.name}</div>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
             <div>
-               {contact.name} 
+              <div>{contact.name}</div>
+              <div>{contact.email}</div>
+              <div>{contact.phone}</div>
+              <div>{contact.address}</div>
             </div>
-            <div>
-                {contact.email}
-            </div>
-            <div>
-               {contact.phone} 
-            </div>
-            <div>
-                {contact.address}
-            </div>
-            
-        </div>
-        
-    );
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
 };
 
-export default ContactCard
+export default ContactCard;
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,29 +1,19 @@
 import React, { useState } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
 import ContactCard from './ContactCard';
 
 
 
 
 const ContactList = (props) =>{
-  const [contacts, setContacts]   = useState([{name:'Name', email: 'email@wherever.com', phone: '(800)555-0000', address:'13 Anywhere street, Everytown, tn 37334'}
-
+  const [contacts, setContacts]   = useState([
+    {name:'Name', email: 'email@wherever.com', phone: '(800)555-0000', address:'13 Anywhere street, Everytown, tn 37334'}
   ]);
+
   return(
         <container className='contactList'>
             <h2>My Contacts</h2>
-            <Accordion >
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey="0">
-               Contact name, image, and buttons here
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>full contact card here</Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-          <ContactCard contact = {{name:'Name', email: 'email@wherever.com', phone: '(800)555-0000', address:'13 Anywhere street, Everytown, tn 37334'}}/>
+            {contacts.map(contact=> <ContactCard contact={contact} />)}
+         
         </container>
     )
 };
