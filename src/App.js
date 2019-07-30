b54b29f66b9f5f4a2f7bf45581d25cc704d7e67a
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-07-30 14:22:46 -0500

    updated toml file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <div>
       <LoginForm />
+      <ProtectedRoute exact path='UserHomePage' component={UserHomePage}/>
       {/* <UserHomePage/> */}
     </div>
   );
