51c6520c20799f4f6e0ca2778a14f2b3c0ae5b04
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-31 17:58:23 -0500

    everything linked; functionality in place

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import ContactsForm from './ContactsForm'
+import EditContactsForm from './EditContactsForm'
 import { Route, Link } from 'react-router-dom';
 
 
@@ -26,13 +27,19 @@ const ContactCard = props => {
           </Accordion.Toggle>
         <Accordion.Collapse eventKey="0">
           <Card.Body>
+            {props.editing ? (
+            <div> <EditContactsForm editing={props.editing}
+								setEditing={props.setEditing}
+								currentContact={props.currentContact}
+								updateContact={props.updateContact}/></div>
+            ) : (
             <div>
               <div>{contact.name}</div>
               <div>{contact.email}</div>
               <div>{contact.phone}</div>
               <div>{contact.address}</div>
             </div>
-            <ContactsForm />
+            )}
           </Card.Body>
         </Accordion.Collapse>
         
