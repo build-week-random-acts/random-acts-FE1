242a8555fd51e22f1d3a81d8ed79e41daae8e1be
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-01 10:52:19 -0500

    randomizer WIP

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,2 +1,3 @@
 //function that randomizes.  chooses a contact and an act renders that to screen
 //modal with button to accept act or choose another act different contact
+//https://stackoverflow.com/questions/37167264/javascript-output-random-object-from-array-of-objects
